d9d30f0589d64498d5eb4a8bdb6872a9657f7e9c
Najete Benmansour <najeteb@seas.upenn.edu>
2021-12-22 21:47:25 -0500

    ok

--- code/run-all.R
+++
@@ -1,9 +0,0 @@
-# run all steps of the analysis pipeline
-
-#running the 0-download is not possible because of the API calls
-source("1-cleaning.R")
-source("2-exploration.Rmd")
-source("3-train-test-split.Rmd")
-source("4-regression-modeling.Rmd")
-source("5-tree-modeling.Rmd")
-source("6-model-evaluation.Rmd")
