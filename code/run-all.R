11199d4f6cb8ebc752cb7035054941a8862d7a6e
Najete Benmansour <najeteb@seas.upenn.edu>
2021-12-22 21:20:23 -0500

    final project

--- code/run-all.R
+++ code/run-all.R
@@ -1,8 +1,9 @@
 # run all steps of the analysis pipeline
-source("code/0-download.R")
-source("code/1-cleaning.R")
-source("code/2-exploration.R")
-source("code/3-train-test-split.R")
-source("code/4-regression-modeling.R")
-source("code/5-tree-modeling.R")
-source("code/6-model-evaluation.R")
+
+#running the 0-download is not possible because of the API calls
+source("1-cleaning.R")
+source("2-exploration.Rmd")
+source("3-train-test-split.Rmd")
+source("4-regression-modeling.Rmd")
+source("5-tree-modeling.Rmd")
+source("6-model-evaluation.Rmd")
